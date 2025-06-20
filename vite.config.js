003fbe022d2d20dfefe57527085cb0397e563128
Ali Farahat <ali.farahat@gmail.com>
2025-06-20 14:39:20 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.js'),
       name: 'MilkdownEditor',
       formats: ['umd'],
-      fileName: (format) => `milkdown-kit-bundle.${format}.js`,
+      fileName: (format) => `milkdown-crepe-bundle.umd.js`,
     },
     outDir: 'dist',
     emptyOutDir: true,
