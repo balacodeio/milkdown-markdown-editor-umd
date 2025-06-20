51013a410f25c1d8084003bd771a08abf51b3739
Ali Farahat <ali.farahat@gmail.com>
2025-06-20 13:55:18 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 // src/index.js
 
-// src/index.js
+// Import the Editor from @milkdown/kit
+const { Editor } = require('@milkdown/kit');
 
-// Import the Editor from @milkdown/kit/lib/core.js
-const { Editor } = require('@milkdown/kit/lib/core.js');
+// Import official plugins
+const { history } = require('@milkdown/plugin-history');
+const { clipboard } = require('@milkdown/plugin-clipboard');
+const { cursor } = require('@milkdown/plugin-cursor');
+const { listener } = require('@milkdown/plugin-listener');
+const { indent } = require('@milkdown/plugin-indent');
+const { upload } = require('@milkdown/plugin-upload');
+const { block } = require('@milkdown/plugin-block');
+const { tooltip } = require('@milkdown/plugin-tooltip');
+const { slash } = require('@milkdown/plugin-slash');
+const { collab } = require('@milkdown/plugin-collab');
+const { prism } = require('@milkdown/plugin-prism');
+const { emoji } = require('@milkdown/plugin-emoji');
 
-// Export a function that creates a basic editor instance
+
+// Export a function that creates the editor with all plugins
 module.exports = (options) => {
-  return Editor.make(options).create();
+  return Editor.make(options)
+    .use(history)
+    .use(clipboard)
+    .use(cursor)
+    .use(listener)
+    .use(indent)
+    .use(upload)
+    .use(block)
+    .use(tooltip)
+    .use(slash)
+    .use(collab)
+    .use(prism)
+    .use(emoji)
+    .create();
 };
